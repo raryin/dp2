9666bec5c7f687f8af43cd971b90c32433db9dac
James Moran <James Moran>
2016-09-28 09:20:41 +1000

    Added timing diagnostics to server

--- DP2PHPServer/Program.cs
+++ DP2PHPServer/Program.cs
@@ -12,7 +12,22 @@ namespace DP2PHPServer
         {
             DatabaseAccess dbconnect = new DatabaseAccess();
 
-            dbconnect.Select(DatabaseTable.Receipt, -1);
+            double[] elapsedMs = new double[20];
+
+            for (int i = 0; i < 20; i++)
+            {
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+
+                dbconnect.Select(DatabaseTable.Receipt, -1);
+
+                watch.Stop();
+                elapsedMs[i] = watch.ElapsedMilliseconds;
+            }
+
+            foreach (double d in elapsedMs)
+            {
+                Console.WriteLine(d + "ms");
+            }
 
             ServerHandler.CreateServer(System.Net.IPAddress.Any, 25565);
 
